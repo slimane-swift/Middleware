2c814d88c20bf5be4861cfadba179174d244d89e
noppoman <yuki@miketokyo.com>
2016-05-11 02:19:57 +0900

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Middleware",
     dependencies: [
-        .Package(url: "https://github.com/noppoMan/HTTP.git", majorVersion: 0, minor: 5),
-        .Package(url: "https://github.com/slimane-swift/JSON.git", majorVersion: 0, minor: 6)
+        .Package(url: "https://github.com/noppoMan/HTTP.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/slimane-swift/JSON.git", majorVersion: 0, minor: 7)
     ]
 )
